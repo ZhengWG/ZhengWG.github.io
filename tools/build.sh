32592c45a6bcf31bd994a330a620ca9b35a184b3
ZhengWG <zwg0606@gmail.com>
2021-07-04 23:01:17 +0800

    add build tools for org->markdown && img upload function

---
+++ tools/build.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+#
+# 基于_drafts下的文章build为正式发布文章_posts
+
+set -eu
+# set -x
+
+input_dir='_drafts'
+post_dir='_posts'
+cache_dir='_drafts/tmp'
+mkdir -p $cache_dir
+# 远程图片库
+remote_url='https://github.com/ZhengWG/Imgs_blog/raw/master/'
+# 本地图片库
+local_repo='~/Project/git_work/projects/blog/Imgs_blog'
+
+# 修改文件名
+function rename() {
+  title=`cat $local_file | grep 'title' | head -n1 | sed 's#title: ##' | sed 's# #_#'`
+  echo $title
+  time=`cat $local_file | grep 'date' | head -n1 | sed 's#date: ##' | sed 's# [1-9][1-9]:.*##'`
+  echo $time
+  out_name=${time}-${title}.md
+  out_path=${input_dir}/${out_name}
+  if [ "$local_file" != "$out_path" ];
+  then
+    cp $local_file $out_path
+    mv $local_file ${cache_dir}
+    local_file=$out_path
+  fi
+}
+
+# Table of Contents -> 目录
+function fix_table_name() {
+  sed -i '.tmp.back' 's#Table of Contents#目录#' $local_file
+  mv ${local_file}.tmp.back $cache_dir
+}
+
+# 上传本地图片到远程，并修改图片路径为远程路径
+function upload_img() {
+  chmod a+x tools/scripts/upload_img.py
+  tools/scripts/upload_img.py -i $local_file
+}
+
+# clean _drafs dir
+function clean() {
+  # 复制修改好的文件到发布目录
+  refined_dir=${input_dir}/refined
+  if [ -d $refined_dir ];
+  then
+    mv ${refined_dir}/* ${post_dir}
+  fi
+  # 删除_drafts文件夹下的文件
+  rm -rf ${input_dir}/*
+}
+
+for f in `ls $input_dir | grep -E ".md$|.markdown$"`;
+do
+  local_file=${input_dir}/${f}
+  fix_table_name
+  rename
+  upload_img
+done
+clean
---
+++ tools/scripts/upload_img.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@author:    Wengang.Zheng
+@email:     zwg0606@gmail.com
+@fielName:  upload_img.py
+@Time:      2021-07-01-22:57:45
+@Des:
+'''
+import os
+import re
+import shutil
+import argparse
+import logging
+from urllib.parse import quote
+from git import Repo
+
+
+# 图片仓
+# FIXME: Replaced by cloud
+
+REMOTE_URL = 'https://github.com/ZhengWG/Imgs_blog/raw/master/'
+LOCAL_REP = os.path.realpath(
+    '/Users/zhengwengang/Project/git_work/projects/blog/Imgs_blog')
+IMAGE_FORMATS = ['jpg', 'png', 'jpeg', 'gif']
+
+
+def build_logger():
+    logger = logging.getLogger(__file__)
+    # Create handlers
+    c_handler = logging.StreamHandler()
+    c_handler.setLevel(logging.WARNING)
+    # Create formatters and add it to handlers
+    c_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    c_handler.setFormatter(c_format)
+    # Add handlers to the logger
+    logger.addHandler(c_handler)
+    return logger
+
+
+class ImageUploader():
+    def __init__(self, input_file, local_rep=LOCAL_REP):
+        self.input_file = input_file
+        self.matched_lines = list()
+        self.added_images = list()
+        self.local_rep = local_rep
+        input_name = os.path.splitext(
+            os.path.basename(input_file))[0]
+        self.local_dir = os.path.join(local_rep, input_name)
+        self.repo = Repo(local_rep)
+        os.makedirs(self.local_dir, exist_ok=True)
+
+    def _add(self, local_path):
+        # copy local img to local rep dir
+        local_name = os.path.basename(local_path)
+        rep_path = os.path.join(self.local_dir, local_name)
+        if os.path.exists(rep_path):
+            logger.warning("{} exited, skiped!".format(rep_path))
+            return rep_path
+        shutil.copy(local_path, rep_path)
+        return rep_path
+
+    def _upload(self, remote_rep):
+        if remote_rep[-1] == '/':
+            remote_rep = remote_rep[:-1]
+        # upload to remote rep using git
+        if self.repo.untracked_files:
+            index = self.repo.index
+            index.add(['*'])
+            upload_filename = os.path.basename(self.local_dir)
+            mes_commit = 'add imgs for {}'.format(upload_filename)
+            index.commit(mes_commit)
+            remote = self.repo.remote()
+            remote.push()
+        assert len(self.repo.untracked_files) == 0, 'Upload imgs failed!'
+
+        out_data = dict()
+        # get remote url for every image
+        for line_data in self.matched_lines:
+            # get remote url for single image
+            rep_path = line_data['rep_path']
+            relative_path = rep_path.replace(self.local_rep, '', 1)
+            remote_url = remote_rep + relative_path
+            remote_url = ImageUploader.url_transfer(remote_url)
+            ori_path = line_data['ori_path']
+            ori_data = line_data['ori_data']
+            rem_data = ori_data.replace(ori_path, remote_url, 1)
+            line_data['out_data'] = rem_data
+            out_data[line_data['ori_idx']] = rem_data
+        return out_data
+
+    def process(self, remote_rep, pattern):
+        for idx, line in enumerate(ImageUploader.read_lines(self.input_file)):
+            if ImageUploader.match_pattern(line, pattern):
+                image_path = ImageUploader.split_path(line)
+                rep_path = self._add(image_path)
+                line_data = {
+                    'ori_idx': idx,
+                    'ori_data': line,
+                    'ori_path': image_path,
+                    'rep_path': rep_path
+                }
+                self.matched_lines.append(line_data)
+
+        # upload images to remote_url
+        out_data = self._upload(remote_rep)
+        return out_data
+
+    @staticmethod
+    def split_path(line):
+        local_image_format = r'\(.*g\)'
+        match_line = re.search(local_image_format, line).group()
+        return match_line[1:-1]
+
+    @staticmethod
+    def url_transfer(url):
+        return quote(url, safe=";/?:@&=+$,", encoding='utf-8')
+
+    @staticmethod
+    def read_lines(input_file, striped=False):
+        for line in open(input_file, 'r'):
+            if striped:
+                yield line.strip()
+            else:
+                yield line
+
+    @staticmethod
+    def match_pattern(data, pattern, flags=re.I):
+        return re.search(pattern, data, flags=flags)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='upload img to remote repo')
+    parser.add_argument('--input', '-i', type=str, required=True, help='Input file for uploading img')
+    parser.add_argument('--output', '-o', type=str, default=None, help='Output file for uploading img')
+    return parser.parse_args()
+
+
+def main(args):
+    pattern_list = ["\[.*\].*\.{}".format(fo) for fo in IMAGE_FORMATS]
+    pattern = r""
+    for p in pattern_list:
+        pattern = pattern + p + '|'
+    pattern = pattern[:-1]
+    image_uploader = ImageUploader(args.input)
+    match_lines = image_uploader.process(remote_rep=REMOTE_URL, pattern=pattern)
+
+    with open(args.output, 'w') as f:
+        for idx, line in enumerate(ImageUploader.read_lines(args.input)):
+            line = match_lines.get(idx, line)
+            f.write("{}".format(line))
+
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    logger = build_logger()
+    if args.output is None:
+        input_dir = os.path.dirname(args.input)
+        file_name = os.path.basename(args.input)
+        args.output = os.path.join(input_dir, 'refined', file_name)
+        os.makedirs(os.path.dirname(args.output), exist_ok=True)
+
+    main(args)
